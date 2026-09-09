e97ef4637c6f5a9c7070a53ed131aadb281530c2
Praliz <d.thinggaard18@gmail.com>
2026-09-09 13:03:31 +0200

    finaly the public copy works

--- src/main.py
+++ src/main.py
@@ -1,13 +1,30 @@
-import os, stat
+import os
 import shutil
 
+source_dir = "Static"
+dest_dir = "public"
 def main():
-    if os.path.exists("/mnt/c/Users/SimonThinggaard/workspace/bootdotdev/static_web/public"):
-        shutil.rmtree("/mnt/c/Users/SimonThinggaard/workspace/bootdotdev/static_web/public")
-        os.mkdir("/mnt/c/Users/SimonThinggaard/workspace/bootdotdev/static_web/public",)
+    cleaning()
+    recurse_static(source_dir,dest_dir)
 
-def recurse_static():
-    dir_list = os.listdir("/mnt/c/Users/SimonThinggaard/workspace/bootdotdev/static_web/Static")
-    print(dir_list)
+def cleaning():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+        os.mkdir("public")
+    else:
+        os.mkdir("public")
+
+def recurse_static(source_dir , dest_dir):
+    if not os.path.exists(dest_dir):    
+        os.mkdir(dest_dir)
+    for file_name in os.listdir(source_dir):
+        source_path = os.path.join(source_dir,file_name)
+        destination_path = os.path.join(dest_dir,file_name)
+        if os.path.isfile(source_path):
+            print(f"this is a file and its transferred {source_path} -> {destination_path}")
+            shutil.copy(source_path,destination_path)
+        else:
+            print(f"this is not a file{source_path}")
+            recurse_static(source_path,destination_path)
 main()
-recurse_static()
+
